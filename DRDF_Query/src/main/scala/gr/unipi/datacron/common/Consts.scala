effcfa097b093acbd21c77d6529b538e0ebc40e5
Panagiotis Nikitopoulos <panosbobos@gmail.com>
2017-04-10 08:33:05 -0700

    Added final keyword in Constants class

--- DRDF_Query/src/main/scala/gr/unipi/datacron/common/Consts.scala
+++ DRDF_Query/src/main/scala/gr/unipi/datacron/common/Consts.scala
@@ -2,92 +2,92 @@ package gr.unipi.datacron.common
 
 object Consts {
   //Query file params
-  val qfpQueryName = "name"
-  val qfpQueryType = "query_type"
+  final val qfpQueryName = "name"
+  final val qfpQueryType = "query_type"
 
-  val qfpLatLower = "query.spatio_temporal_predicate.lat_lower"
-  val qfpLonLower = "query.spatio_temporal_predicate.lon_lower"
-  val qfpLatUpper = "query.spatio_temporal_predicate.lat_upper"
-  val qfpLonUpper = "query.spatio_temporal_predicate.lon_upper"
-  val qfpTimeLower = "query.spatio_temporal_predicate.time_lower"
-  val qfpTimeUpper = "query.spatio_temporal_predicate.time_upper"
+  final val qfpLatLower = "query.spatio_temporal_predicate.lat_lower"
+  final val qfpLonLower = "query.spatio_temporal_predicate.lon_lower"
+  final val qfpLatUpper = "query.spatio_temporal_predicate.lat_upper"
+  final val qfpLonUpper = "query.spatio_temporal_predicate.lon_upper"
+  final val qfpTimeLower = "query.spatio_temporal_predicate.time_lower"
+  final val qfpTimeUpper = "query.spatio_temporal_predicate.time_upper"
 
-  val qfpTripleS = "query.star_predicate.s"
-  val qfpTripleP = "query.star_predicate.p"
-  val qfpTripleO = "query.star_predicate.o"
+  final val qfpTripleS = "query.star_predicate.s"
+  final val qfpTripleP = "query.star_predicate.p"
+  final val qfpTripleO = "query.star_predicate.o"
 
-  val qfpTotalBits = "universe.nTotalBits"
-  val qfpSpatialBits = "universe.nSpatialBits"
-  val qfpIDsBits = "universe.nIDsBits"
-  val qfpUniverseLatLower = "universe.lat_lower"
-  val qfpUniverseLonLower = "universe.lon_lower"
-  val qfpUniverseLatUpper = "universe.lat_upper"
-  val qfpUniverseLonUpper = "universe.lon_upper"
+  final val qfpTotalBits = "universe.nTotalBits"
+  final val qfpSpatialBits = "universe.nSpatialBits"
+  final val qfpIDsBits = "universe.nIDsBits"
+  final val qfpUniverseLatLower = "universe.lat_lower"
+  final val qfpUniverseLonLower = "universe.lon_lower"
+  final val qfpUniverseLatUpper = "universe.lat_upper"
+  final val qfpUniverseLonUpper = "universe.lon_upper"
 
-  val qfpDicPath = "datasets.dictionary.path"
-  val qfpTriplesPath = "datasets.triples.path"
-  val qfpIntrvlsPath = "datasets.intervals.path"
+  final val qfpDicPath = "datasets.dictionary.path"
+  final val qfpTriplesPath = "datasets.triples.path"
+  final val qfpIntrvlsPath = "datasets.intervals.path"
 
-  val qfpParseDictionary = "datasets.dictionary.parse"
-  val qfpParseTriples = "datasets.triples.parse"
+  final val qfpParseDictionary = "datasets.dictionary.parse"
+  final val qfpParseTriples = "datasets.triples.parse"
 
-  val qfpSparkMaster = "execution.spark_master"
+  final val qfpSparkMaster = "execution.spark_master"
 
-  val qfpDictionaryTrait = "plans.physical.dictionary"
-  val qfpTriples_trait = "plans.physical.triples"
-  val qfpJoinTriples_trait = "plans.physical.joinTriples"
+  final val qfpDictionaryTrait = "plans.physical.dictionary"
+  final val qfpTriples_trait = "plans.physical.triples"
+  final val qfpJoinTriples_trait = "plans.physical.joinTriples"
 
-  val qfpLogicalPlans = "plans.logical"
+  final val qfpLogicalPlans = "plans.logical"
 
   //Query types
-  val starSptRangeQuery = "StarSptRange"
-  val twoHopSptRangeQuery = "TwoHopSptRange"
+  final val starSptRangeQuery = "StarSptRange"
+  final val twoHopSptRangeQuery = "TwoHopSptRange"
 
   //Logical plans
-  val spatialFirstStarSptRangeLPlan = "SpatialFirstStarSptRange"
-  val spatialFirstJoinStarSptRangeLPlan = "SpatialFirstJoinStStarSptRange"
-  val rdfFirstStarSptRangeLPlan = "RdfFirstStarSptRange"
+  final val spatialFirstStarSptRangeLPlan = "SpatialFirstStarSptRange"
+  final val spatialFirstJoinStarSptRangeLPlan = "SpatialFirstJoinStStarSptRange"
+  final val rdfFirstStarSptRangeLPlan = "RdfFirstStarSptRange"
 
   //Physical plans
   //TDictionary
-  val tLSDictionary = "LSDictionary"
-  val tSDictionary = "SDictionary"
+  final val tLSDictionary = "LSDictionary"
+  final val tSDictionary = "SDictionary"
 
   //TTriples
-  val tSTriples = "STriples"
-  val tLLLTriples = "LLLTriples"
+  final val tSTriples = "STriples"
+  final val tLLLTriples = "LLLTriples"
 
   //TJoinTriples
-  val tMBJoinSTriples = "MBJoinSTriples"
-  val tMBJoinLLLTriples = "MBJoinLLLTriples"
+  final val tMBJoinSTriples = "MBJoinSTriples"
+  final val tMBJoinLLLTriples = "MBJoinLLLTriples"
   
   //field separators
-  val tripleFieldsSeparator = " "
-  val dicFieldsSeparator = "\t"
-  val lonLatSeparator = " "
+  final val tripleFieldsSeparator = " "
+  final val dicFieldsSeparator = "\t"
+  final val lonLatSeparator = " "
   
   //RDF URIs
-  val uriMBR = ":hasMBR_WKT"
-  val uriTime = ":SemanticNodeTimeStart"
+  final val uriMBR = ":hasMBR_WKT"
+  final val uriTime = ":SemanticNodeTimeStart"
   
   //Triples fields
-  val tripleSpoStrField = "spoStr"
-  val triplePruneSubKeyField = "pruneSubKey"
-  val tripleSubLongField = "subLong"
-  val triplePredLongField = "predLong"
-  val tripleObjLongField = "objLong"
-  val tripleTranslateSuffix = "_trans"
-  val tripleMBRField = "mbr_wkt"
-  val tripleTimeStartField = "time_start"
+  final val tripleSpoStrField = "spoStr"
+  final val triplePruneSubKeyField = "pruneSubKey"
+  final val tripleSubLongField = "subLong"
+  final val triplePredLongField = "predLong"
+  final val tripleObjLongField = "objLong"
+  final val tripleTranslateSuffix = "_trans"
+  final val tripleMBRField = "mbr_wkt"
+  final val tripleTimeStartField = "time_start"
   
   //Dictionary fields
-  val dicLineStrField = "lineStr"
-  val dicKeyLongField = "keyLong"
-  val dicValueStrField = "valueStr"
+  final val dicLineStrField = "lineStr"
+  final val dicKeyLongField = "keyLong"
+  final val dicValueStrField = "valueStr"
 
   //parsing
-  val parseString = "S"
-  val parseLongString = "LS"
-  val parseTripleLong = "LLL"
+  final val parseString = "S"
+  final val parseLongString = "LS"
+  final val parseTripleLong = "LLL"
 }
 
